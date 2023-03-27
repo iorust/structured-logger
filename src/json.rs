60882996a51192b5e80f06037d5c3718c4403fef
zensh <txr1883@gmail.com>
2023-03-27 18:12:44 +0800

    fix RefCell borrow_mut() panicking.

--- src/json.rs
+++ src/json.rs
@@ -5,16 +5,19 @@ use std::{cell::RefCell, io};
 
 use crate::{Log, Writer};
 
+use parking_lot::Mutex;
+
+// TODO: async writer.
 /// A Writer implementation that writes logs in JSON format.
-pub struct JSONWriter<W: io::Write + Sync + Send>(RefCell<Box<W>>);
+pub struct JSONWriter<W: io::Write + Sync + Send>(Mutex<RefCell<Box<W>>>);
 
 /// Implements Writer trait for JSONWriter.
 impl<W: io::Write + Sync + Send> Writer for JSONWriter<W> {
     fn write_log(&self, value: &Log) -> Result<(), io::Error> {
-        serde_json::to_writer(self.0.borrow_mut().as_mut(), value).map_err(io::Error::from)?;
+        let w = self.0.lock();
+        serde_json::to_writer(w.borrow_mut().as_mut(), value).map_err(io::Error::from)?;
         // must write the LINE FEED character.
-        self.0
-            .borrow_mut()
+        w.borrow_mut()
             .as_mut()
             .write_all(b"\n")
             .map_err(io::Error::from)?;
@@ -24,5 +27,5 @@ impl<W: io::Write + Sync + Send> Writer for JSONWriter<W> {
 
 /// Creates a new JSONWriter instance with a given std::io::Write instance.
 pub fn new_json_writer<W: io::Write + Sync + Send + 'static>(w: W) -> Box<dyn Writer> {
-    Box::new(JSONWriter(RefCell::new(Box::new(w))))
+    Box::new(JSONWriter(Mutex::new(RefCell::new(Box::new(w)))))
 }
